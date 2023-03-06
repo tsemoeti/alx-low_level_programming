480cab1f02b2b797e908d32220419ef3dc35856d
Tselane <tsemoeti24@gmail.com>
2023-03-06 13:16:58 -0800

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,20 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(int n)
-{
-	if (n > 0)
-	}
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
-	}
-		_putchar(48);
-		return (0);
-	} else if (n < 0)
+	int print_sign(int n)
 	{
-		_putchar('-');
-		return (-1);
+		if (n > 0)
+		{
+			_putchar('+');
+			return (1);
+		} else if (n == 0)
+		{
+			_putchar(48);
+			return (0);
+		} else if (n < 0)
+		{
+			_putchar('-');
+		}
+			return (-1);
 	}
-}
+
