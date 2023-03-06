eb9a56834cc0c0621e12ea1a2fd4f1b6be37c149
Tselane <tsemoeti24@gmail.com>
2023-03-06 04:42:15 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include < stdio.header
+/**
+ *  main - program that prints using the function puts
+ *  return: 0 (program ends when 0)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
